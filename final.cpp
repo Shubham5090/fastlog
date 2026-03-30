80e3ce201e2e32961a6da732fcaa56ffacd883f9
AbinavKrishna15 <abinavkrishna15@gmail.com>
2026-03-30 18:53:34 +0530

    Issue #5 - Improve File Reading Speed

--- final.cpp
+++ final.cpp
@@ -10,7 +10,6 @@ struct Log {
 
 inline Log parse_line(const string& line) {
     Log log;
-
     const char* p = line.c_str();
 
     // skip timestamp
@@ -53,22 +52,49 @@ int main(int argc, char* argv[]) {
         cout << "Error opening file\n";
         return 1;
     }
-
     vector<Log> logs;
+
     logs.reserve(5000000);
+    const int CHUNK_SIZE = 8 * 1024 * 1024; // 8 MB
+    vector<char> buffer(CHUNK_SIZE);
+    string leftover;
 
-    string line;
+    // -------- READ--------
 
-    // -------- READ --------
     auto read_start = steady_clock::now();
+    while (file) {
+        file.read(buffer.data(), CHUNK_SIZE);
+        streamsize bytes_read = file.gcount();
+        if (bytes_read == 0) break;
+        const char* chunk_start = buffer.data();
+        const char* chunk_end   = chunk_start + bytes_read;
+        const char* p           = chunk_start;
+        while (p < chunk_end) {
+            const char* nl = (const char*)memchr(p, '\n', chunk_end - p);
+            if (!nl) {
+                leftover.append(p, chunk_end - p);
+                break;
+            }
+            if (!leftover.empty()) {
+                leftover.append(p, nl - p);
+                logs.push_back(parse_line(leftover));
+                leftover.clear();
+            } else {
+                logs.emplace_back(parse_line(string(p, nl)));
+            }
+            p = nl + 1;
+        }
+    }
 
-    while (getline(file, line)) {
-        logs.push_back(parse_line(line));
+    if (!leftover.empty()) {
+        logs.push_back(parse_line(leftover));
+        leftover.clear();
     }
 
     auto read_end = steady_clock::now();
 
     // -------- PROCESS --------
+
     auto proc_start = steady_clock::now();
 
     unordered_map<string, int> level_count;
@@ -86,6 +112,7 @@ int main(int argc, char* argv[]) {
     }
 
     // Average
+
     double avg_latency = (double)total_latency / logs.size();
 
     // Top 10 users
